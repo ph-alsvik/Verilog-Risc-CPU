90fac89fe211bca0f35b21567b21d86f67a76ab5
Gannondorf <pealsvik@gmail.com>
2025-12-21 22:10:45 +0100

    Changes made to alu inverter

--- src/alu/inverter.v
+++ src/alu/inverter.v
@@ -3,6 +3,6 @@ module inverter (
 	output [15:0] Y 
 );
 
-	assign Y = {~A[15], ~A[14], ~A[13], ~A[12], ~A[11], ~A[10], ~A[9], ~A[8], ~A[7], ~A[6], ~A[5], ~A[4], ~A[3], ~A[2], ~A[1], ~A[0]};
+	assign Y = ~A;
 
 endmodule
